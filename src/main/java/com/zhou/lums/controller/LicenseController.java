e1d81b109028e350ab552d7a4b01e49c2ac45e09
ysz951 <shengyang_zhou@hotmail.com>
2021-02-10 15:36:17 -0600

    add licnese serice

--- src/main/java/com/zhou/lums/controller/LicenseController.java
+++ src/main/java/com/zhou/lums/controller/LicenseController.java
@@ -1,12 +1,18 @@
 package com.zhou.lums.controller;
 
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.zhou.lums.exception.ResourceNotFoundException;
 import com.zhou.lums.model.License;
+import com.zhou.lums.model.License.Duration;
 import com.zhou.lums.respository.LicenseRepository;
 import com.zhou.lums.service.LicenseService;
 
@@ -23,5 +29,22 @@ public class LicenseController {
     @PostMapping
     public License createLicense(@Valid @RequestBody License license) {
         return licenseRepository.save(license);
+    }
+
+    @GetMapping
+    public List<License> listAllLicense() {
+        return licenseRepository.findAll();
+    }
+
+    @GetMapping("/{licenseId}")
+    public License listLicenseById(@PathVariable(value="licenseId") long licenseId) {
+        return licenseRepository.findById(licenseId)
+                .orElseThrow(() -> new ResourceNotFoundException("License", "id", licenseId));
+    }
+
+
+    @GetMapping("/duration")
+    public List<License> listAllLicenseByDuration(@RequestParam(value = "duration") Duration duration) {
+        return licenseRepository.findAllOrderedByIdFilterByType(duration);
     }
 }
