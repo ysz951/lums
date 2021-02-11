6b65d94686e2e3b64cd65f81bda6d6658d7e799b
ysz951 <shengyang_zhou@hotmail.com>
2021-02-11 00:00:01 -0600

    license active

--- src/main/java/com/zhou/lums/service/LicenseService.java
+++ src/main/java/com/zhou/lums/service/LicenseService.java
@@ -3,29 +3,23 @@ package com.zhou.lums.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import com.zhou.lums.exception.ResourceNotFoundException;
-import com.zhou.lums.model.License;
+import org.springframework.transaction.annotation.Transactional;
 import com.zhou.lums.payload.ApiResponse;
 import com.zhou.lums.respository.LicenseRepository;
 
 @Service
+@Transactional
 public class LicenseService {
 
     @Autowired
     private LicenseRepository licenseRepository;
     public ResponseEntity<?> changeLicenseActive(long licenseId, boolean newActive) {
-        License license = licenseRepository.findById(licenseId)
-                .orElseThrow(() -> new ResourceNotFoundException("License", "id", licenseId));
-        license.setActive(newActive);
-        licenseRepository.save(license);
-        return ResponseEntity.ok(new ApiResponse(true, "Change sale exipration"));
+//        License license = licenseRepository.findById(licenseId)
+//                .orElseThrow(() -> new ResourceNotFoundException("License", "id", licenseId));
+//        license.setActive(newActive);
+//        licenseRepository.save(license);
+        licenseRepository.updateLicenseActive(newActive, licenseId);
+        return ResponseEntity.ok(new ApiResponse(true, "Change License Active"));
     }
- //    public ResponseEntity<?> changeSaleExpiration(long saleId, LocalDate expireDate) {
-//        Sale sale = saleRepository
-//                .findById(saleId)
-//                .orElseThrow(() -> new ResourceNotFoundException("Sale", "id", saleId));
-//        sale.setExpireDate(expireDate);
-//        saleRepository.save(sale);
-//        return ResponseEntity.ok(new ApiResponse(true, "Change sale exipration"));
-//    }
+
 }
