27994591e1d411e401fb260d0b6f01d45d6b3ef5
ysz951 <shengyang_zhou@hotmail.com>
2021-03-09 17:51:07 -0600

    Change init data

---
+++ src/test/java/com/zhou/lums/BCryptPasswordEncoderTest.java
@@ -0,0 +1,14 @@
+package com.zhou.lums;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+public class BCryptPasswordEncoderTest {
+    @Test
+    public void testPasswordEncode() {
+        // user123
+        String encoded=new
+                BCryptPasswordEncoder().encode("admin123");
+        System.out.println(encoded);
+    }
+}
