6b14558d9fafe130d10dd70d9ebb08d49ac525cd
ysz951 <shengyang_zhou@hotmail.com>
2021-03-15 15:18:43 -0500

    Add web tests

---
+++ src/test/java/com/zhou/lums/WebTests.java
@@ -0,0 +1,76 @@
+package com.zhou.lums;
+
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import com.zhou.lums.payload.LoginRequest;
+
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SuppressWarnings("unchecked")
+public class WebTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private static String token;
+
+    @BeforeEach
+    public void loginTest() {
+        LoginRequest login = new LoginRequest();
+        HttpEntity<LoginRequest> request = new HttpEntity<>(login);
+        login.setUsernameOrEmail("lums@admin.com");
+        login.setPassword("admin123");
+        Map<String, ?> response = restTemplate.postForObject("http://localhost:" + port + "/api/auth/signin",
+                request, Map.class);
+        token = (String) response.get("accessToken");
+        System.out.println(response.get("accessToken"));
+    }
+
+//    @Test
+//    public void greetingShouldReturnDefaultMessage() throws Exception {
+//        System.out.println("token is " + token);
+//        List<Map<String, ?>> response = restTemplate.getForObject("http://localhost:" + port + "/api/users",
+//                List.class);
+//        int i = 1;
+//        for (Map user : response) {
+//            System.out.println((int) user.get("id") == i);
+//            System.out.println(user.get("email"));
+//            System.out.println(user.get("blocked"));
+//            i++;
+//        }
+//        System.out.println(response.size());
+//
+//    }
+
+    @Test
+    public void getLicense() throws Exception {
+        System.out.println("license");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Bearer " + token);
+        HttpEntity<?> request = new HttpEntity<>(headers);
+        ResponseEntity<Map> response =restTemplate.exchange("http://localhost:" + port + "/api/user/me", HttpMethod.GET, request, Map.class);
+        System.out.println(response.getBody());
+        Map<String, ?> ob = response.getBody();
+        for (String key : ob.keySet()) {
+            System.out.println(key + " : " + ob.get(key));
+        }
+    }
+
+
+}
